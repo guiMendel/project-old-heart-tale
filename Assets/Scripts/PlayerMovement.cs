1220b476ce1279ecda9d45ca64e4eb6424f677d3
guimendel <guimendel@gmail.com>
2023-04-05 19:44:31 -0300

    ✨Initial: simple movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+  // === PARAMS
+
+  public float speed = 5f;
+
+  // === STATE
+
+  Vector2 direction;
+
+  private void Update()
+  {
+    transform.Translate(direction * speed * Time.deltaTime);
+  }
+
+  public void Move(InputAction.CallbackContext value)
+  {
+    if (value.phase == InputActionPhase.Performed)
+      direction = value.ReadValue<Vector2>();
+
+    else if (value.phase == InputActionPhase.Canceled)
+      direction = Vector2.zero;
+
+  }
+}
